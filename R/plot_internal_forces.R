5ba5a7632833327643a9f362e794db13b97367ab
Javier de Elio Medina <demja@hvl.no>
2023-11-03 22:18:46 +0100

    Include the possibility of ploting punctual moments (.point_moment) in the internal forces diagrams

--- R/plot_internal_forces.R
+++ R/plot_internal_forces.R
@@ -5,6 +5,10 @@
 #' `x` is the position of the punctual force on the beam from the left edge [m].
 #' `force` is the magnitude of the force [N]; positive forces going up, and negative
 #' forces going down. Support reactions should also be added in this data frame.
+#' @param .point_moment A tibble with punctual moments, it must have two columns (x and moment).
+#' `x` is the position of the punctual moment on the beam from the left edge [m].
+#' `moment` is the magnitude of the moment [N*m]; positive moment counter-clockwise rotation,
+#' and negative moment clockwise rotation. Support moments (e.g. fixed support) should also be added in this data frame.
 #' @param .distributed_load A tibble with the distributed load [N/m]. It must have two columns
 #' (x and load), with the load values in steps of x = `.resolution`.
 #' @param .resolution The beam is discretised for the calculations, by default
@@ -34,11 +38,15 @@
 
 plot_internal_forces <- function(.beam_length,
                                 .point_force,
+                                .point_moment = NULL,
                                 .distributed_load = NULL,
                                 .resolution = 0.001) {
 
   # Create points (x) for the internal forces (steps = .resolution)
-  df <- tibble::tibble(x = seq(0, .beam_length, .resolution)) |>
+  df <- tibble::tibble(x = seq(0, .beam_length, .resolution))
+
+  # Shear forces ---
+  df <- df |>
     # Add point force location (.point_loads)
     dplyr::left_join(.point_force) |>
     dplyr::mutate(force = replace_na(force, 0)) |>
@@ -54,124 +62,92 @@ plot_internal_forces <- function(.beam_length,
                   # Shear
                   shear = cumsum(total_disc_force)) |>
     # Convert x again to number
-    dplyr::mutate(x = as.numeric(as.character(x)))
-
-  # Bending moment ----
-  df <- df |>
+    dplyr::mutate(x = as.numeric(as.character(x))) |>
     # Area of the shear
     dplyr::mutate(area_shear = .resolution * shear)
 
-  # Calculate Bending moment
-  M <- matrix(0, length(df$x))
-  for (i in 1:(length(df$x)-1) ) {
-    M[i+1, ] = M[i, ] + df[i+1, ]$area_shear
-  }
-  M <- as.data.frame(M)
-  names(M) <- c("moment")
+  # Bending moments ----
+  if(is.null(.point_moment)) {
 
-  # Add to df
-  df <- df |> dplyr::mutate(moment = M$moment)
+    df <- df |>
+        # Discrete moment
+        dplyr::mutate(moment = cumsum(area_shear))
+
+  } else {
+
+    df <- df |>
+      # Add point moments (.point_moment)
+      dplyr::left_join(.point_moment) |>
+      dplyr::rename(p_moment = moment) |>
+      dplyr::mutate(
+        # Punctual moment
+        p_moment = replace_na(p_moment, 0),
+        # Discrete moment
+        d_moment = area_shear - p_moment,
+        # Moment at x
+        moment = cumsum(d_moment))
+  }
 
   # Plots ----
+
+  p_loads <- ggplot() +
+    # Beam
+    geom_segment(data = df,
+                 aes(x = 0, y = 0, xend = max(x), yend = 0),
+                 linewidth = 4, colour = "darkgrey") +
+    # Punctual forces
+    geom_segment(data = .point_force,
+                 aes(x = x, y = force, xend = x, yend = 0),
+                 arrow = arrow(type = "closed", length = unit(0.08, "inches")),
+                 colour = "blue",
+                 linewidth = 0.75) +
+    scale_y_reverse() +
+    labs(x = "Position [m]",
+         y = "F [N]", title = "Load forces") +
+    theme_light()
+
+  # Plot shear forces
+  p_shear <- ggplot() +
+    geom_hline(yintercept = 0) +
+    geom_line(data = df,
+              aes(x = x, y = shear),
+              colour = "red",
+              linewidth = 0.75) +
+    labs(x = "Position [m]",
+         y = "F [N]",
+         title = "Shear Force Diagram") +
+    theme_light()
+
+  # Plot moment
+  p_moment <- ggplot() +
+    geom_hline(yintercept = 0) +
+    geom_line(data = df,
+              aes(x = x, y = moment),
+              colour = "red",
+              linewidth = 0.75) +
+    labs(x = "Position [m]",
+         y = "M [N*m]",
+         title = "Moment Diagram") +
+    theme_light()
 
   # Only punctual forces
 
   if(is.null(.distributed_load)) {
 
-    # Plots ----
-    # Punctual forces
-    p_loads <- ggplot() +
-      # Beam
-      geom_segment(data = df,
-                   aes(x = 0, y = 0, xend = max(x), yend = 0),
-                   linewidth = 4, colour = "darkgrey") +
-      # Punctual forces
-      geom_segment(data = .point_force,
-                   aes(x = x, y = force, xend = x, yend = 0),
-                   arrow = arrow(type = "closed", length = unit(0.1, "inches")),
-                   colour = "blue",
-                   linewidth = 0.75) +
-      scale_y_reverse() +
-      labs(x = "Position [m]",
-           y = "F [N]", title = "Load forces") +
-      theme_light()
-
-    # Plot shear forces
-    p_shear <- ggplot() +
-      geom_hline(yintercept = 0) +
-      geom_line(data = df,
-                aes(x = x, y = shear),
-                colour = "red",
-                linewidth = 0.75) +
-      labs(x = "Position [m]",
-           y = "F [N]",
-           title = "Shear Force Diagram") +
-      theme_light()
-
-    # Plot moment
-    p_moment <- ggplot() +
-      geom_hline(yintercept = 0) +
-      geom_line(data = df,
-                aes(x = x, y = moment),
-                colour = "red",
-                linewidth = 0.75) +
-      labs(x = "Position [m]",
-           y = "M [N*m]",
-           title = "Moment Diagram") +
-      theme_light()
-
     # Diagrams
     plot_diagrams <- p_loads / p_shear / p_moment
+
 
   } else {
 
     # Plots ----
     # Punctual forces
-    p_loads <- ggplot() +
+    p_loads <- p_loads +
       # Distribute loads
       geom_segment(data = .distributed_load,
                    aes(x = x, y = load, xend = x, yend = 0),
                    colour = "red",
-                   linewidth = 0.75) +
-      # Beam
-      geom_segment(data = df,
-                   aes(x = 0, y = 0, xend = max(x), yend = 0),
-                   linewidth = 2,
-                   colour = "darkgrey") +
-      # Punctual forces
-      geom_segment(data = .point_force,
-                   aes(x = x, y = force, xend = x, yend = 0),
-                   arrow = arrow(type = "closed", length = unit(0.1, "inches")),
-                   colour = "blue",
-                   linewidth = 0.75) +
-      scale_y_reverse() +
-      labs(x = "Position [m]",
-           y = "F [N]", title = "Load forces") +
-      theme_light()
-
-    # Plot shear forces
-    p_shear <- ggplot() +
-      geom_hline(yintercept = 0) +
-      geom_line(data = df,
-                aes(x = x, y = shear),
-                colour = "red",
-                linewidth = 0.75) +
-      labs(x = "Position [m]",
-           y = "F [N]",
-           title = "Shear Force Diagram") +
-      theme_light()
-
-    # Plot moment
-    p_moment <- ggplot() +
-      geom_hline(yintercept = 0) +
-      geom_line(data = df,
-                aes(x = x, y = moment),
-                colour = "red",
-                linewidth = 0.75) +
-      labs(x = "Position [m]",
-           y = "M [N*m]",
-           title = "Moment Diagram") +
-      theme_light()
+                   linewidth = 0.01)
 
     # Diagrams
     plot_diagrams <- p_loads / p_shear / p_moment
